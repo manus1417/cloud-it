77bcd22d456a5e4937e45a84990b58444d792692
Manaswini <128171861+manus1417@users.noreply.github.com>
2025-03-15 15:55:00 +0530

    Update db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { prisma } from "../lib/prisma";
 
+export const db = prisma;
 const createPrismaClient = () => new PrismaClient();
 
 const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
-export const db = globalForPrisma.prisma ?? createPrismaClient();
+
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
